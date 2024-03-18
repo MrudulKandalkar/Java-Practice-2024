7db7941c13b1a728392d1fbac958790fcaafa000
MrudulKandalkar <159232423+MrudulKandalkar@users.noreply.github.com>
2024-03-18 17:53:08 +0530

    Codes For Loop

---
+++ March/ForLoopAlphabets2.java
@@ -0,0 +1,15 @@
+class ForLoopAlphabets2
+{
+	public static void main(String[] args) {
+		
+		for (char ch='Z';ch>='A';ch--) {
+			System.out.print(ch+ " ");
+			
+		}
+		System.out.println();
+		for (char ch='z';ch>='a';ch-- ) {
+			System.out.print(ch+ " ");
+			
+		}
+	}
+}
